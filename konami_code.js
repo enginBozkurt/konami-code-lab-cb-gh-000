3c1d1f6d0a6cbe3e361305b7f1c72fb6dddfdc31
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-16 17:36:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,27 +12,29 @@ const codes = [
 ];
 
 function init() {
-  
+
   // your code here
-  let index = 0;
+let index = 0;
 
 document.body.addEventListener("keydown", (thingPressed) => {
-  const key = thingPressed.key 
-  console.log(key) 
-  
-  
+  const key = thingPressed.key
+  console.log(key)
+
+
   if (codes[index] === key) {
-    ++index 
-  } 
-  
-  else {
-    idx = 0 
+    ++index
   }
+
+  else {
+    idx = 0
+  }
+
+  console.log(index);
   
-  console.log(index)
   if (index == 10) {
     alert("Hurray!");
-    index = 0 
+    index = 0
   }
-}); 
+});
 }
+
